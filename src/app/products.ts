474ea5ebf3dd955ba7c0c3c61aaa32ff8b6ab7fa
Rajbir <rajbir.singh@servercenter.ca>
2020-07-16 04:26:37 -0600

    new

---
+++ src/app/products.ts
@@ -0,0 +1,33 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: 'Rajbir'
+  },
+  {
+    name: 'Bike',
+    price: 7990,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Iphone',
+    price: 1000,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'HP Laptop',
+    price: 2000,
+    description: 'Rajbir'
+	
+  }
+];
